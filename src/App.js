2d1a0398bb81bcbbe518093dabee60752f6d2102
NineOne91 <ihim512@gmail.com>
2021-02-25 09:25:47 +0700

    full project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import 'assets/css/style.css';
+import HomePage from 'scenes/HomePage'
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
